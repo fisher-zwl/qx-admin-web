55a4b174aa21f8bf833ef67dee6755cc8efe382e
WIN-6V66K1VIL6I\long123 <1426034446@qq.com>
2018-06-06 16:54:18 +0800

    首页

--- src/main/Station_abbreviations.vue
+++ src/main/Station_abbreviations.vue
@@ -5,7 +5,7 @@
             <div class="cp">
                 <el-upload
                     class="avatar-uploader cp-image"
-                    action="https://jsonplaceholder.typicode.com/posts/"
+                    :action="action"
                     :show-file-list="false"
                     :on-success="handleAvatarSuccess"
                     :before-upload="beforeAvatarUpload">
@@ -14,14 +14,37 @@
                 </el-upload>
                 <el-form :inline="true" class="cp-name">
                     <el-form-item label="产品名称">
-                        <el-input  placeholder="请输入产品名称"></el-input>
+                        <el-input v-model="name" placeholder="请输入产品名称"></el-input>
                     </el-form-item>
                 </el-form>
                 <el-form :inline="true" class="cp-description">
                     <el-form-item label="产品描述">
-                        <el-input  placeholder="请输入产品描述"></el-input>
+                        <el-input v-model="description" placeholder="请输入产品描述"></el-input>
                     </el-form-item>
                 </el-form>
+                <el-form :inline="true" class="cp-relPro" >
+                    <el-form-item label="关联产品" >
+                        <el-select v-model="relPro" @change="handleChange" placeholder="请选择关联产品">
+                            <el-option
+                                v-for="item in relPro_options"
+                                :key="item.id"
+                                :label="item.title"
+                                :value="item.id">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                </el-form>
+                <el-button type="primary" @click="onSubmit">提交</el-button>
+                <span class="cp-cont-com">效果预览:</span>
+                <ul class="yltp">
+                    <li  v-for="item in suolueData" :key="item.id">
+                        <img src="item.url"/>
+                        <div>
+                            <label>{{item.name}}</label>
+                            <span>{{item.description}}</span>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </el-tab-pane>
         <el-tab-pane label="案例展示">案例展示</el-tab-pane>
@@ -37,11 +60,32 @@ import {Notification,MessageBox} from 'element-ui'
 export default {
   data() {
     return {
-        imageUrl: ''
+        imageUrl: '',
+        relPro:'',
+        relPro_options:[],
+        suolueData:[],
+        action:'',
+        url:'',
+        description:'',
+        name:'',
+        id:0,
     };
   },
   methods: {
+      async getProList(){
+            let r = await axios.get('/products', {})
+            if(r && r.code == 0){
+                this.relPro_options = r.data
+            }else{
+                Notification.error({
+                    title: '错误',
+                    message: '获取《联系我们》菜单失败',
+                    type:'error'
+                })
+            }
+      },
       handleAvatarSuccess(res, file) {
+          this.url = res.data[0]
         this.imageUrl = URL.createObjectURL(file.raw);
       },
       beforeAvatarUpload(file) {
@@ -55,9 +99,53 @@ export default {
           this.$message.error('上传头像图片大小不能超过 2MB!');
         }
         return isJPG && isLt2M;
+      },
+      handleChange(val){
+         this.relPro = val
+      },
+      async onSubmit(){
+          let _this = this
+          let params = {
+              url:_this.url,
+              name:_this.name,
+              description:_this.description,
+              infoId:_this.relPro
+          }
+          if(params.url){
+               Notification.error({
+                    title: '错误',
+                    message: '请选择图片',
+                    type:'error'
+                })
+                return false
+          }
+          if(params.name){
+               Notification.error({
+                    title: '错误',
+                    message: '请输入产品名称',
+                    type:'error'
+                })
+                return false
+          }
+          if(params.description){
+               Notification.error({
+                    title: '错误',
+                    message: '请输入产品描述',
+                    type:'error'
+                })
+                return false
+          }
+          if(_this.id){
+              params.id = _this.id
+              let r = await axios.post('/products/update-ar',{url:params.url,name:params.url,description:params.description,id:params.id})
+          }else{
+              let r = await axios.post('/products/update-ar',{url:params.url,name:params.url,description:params.description})
+          }
       }
   },
   mounted(){
+    this.action = window.location.origin+'/admin/v1/products/upload-ar'
+    this.getProList()
   }
 }
 </script>
@@ -86,16 +174,46 @@ export default {
             height: 178px;
             display: block;
         }
+        .cp .el-form-item .el-form-item__content{
+            width: 300px;
+        }
+        .cp .cp-relPro .el-form-item .el-form-item__content{
+            width: 300px;
+            text-align: left;
+            .el-select{
+                width: 100%;
+            }
+        }
 
         .cp{
+            position: relative;
             .cp-image{
                 width: 400px;
+                margin-bottom: 15px;
             }
             .cp-name{
                 width: 400px;
             }
             .cp-description{
                 width: 400px;
+            }
+            .cp-relPro{
+                 width: 400px;
+            }
+            .el-button{
+                position: absolute;
+                left: 400px;
+                top: 323px;
+            }
+            .cp-cont-com{
+                display: inline-block;
+                width: 100%;
+                text-align: left;
+                margin-left: 10px;
+            }
+            .yltp{
+                width: 100%;
+                border: 1px solid #f2f2f2;
             }
         }
     }
